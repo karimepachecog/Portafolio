5fd19423f38ecd6495faff3741d2662fd8294004
akkarie <a22216265@alumnos.uady.mx>
2022-11-28 22:01:46 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Karime Pacheco",
+  title: "Actuarial Science Student",
+  email: "karime.pacheco.studentambassadors.com",
+  gitHub: "karimepachecog",
+  instagram: "akkarie_",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "akkarie_",
+  youTube: "Akkarie",
 };
 
 const primaryColor = "#4E567E";
